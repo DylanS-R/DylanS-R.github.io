fc575d203e1138dec7db845c9253cb498dae352d
DylanS-R <129183642+DylanS-R@users.noreply.github.com>
2024-05-04 13:39:29 +0800

    blur contact section

--- js/blur.js
+++ js/blur.js
@@ -1,12 +1,16 @@
-const blobs = document.getElementsByClassName("blob");
+const header-blob = document.getElementById("header-blob");
+const contact-blob = document.getElementById("contact-blob");
 
 window.onpointermove = event => {
   const { clientX, clientY } = event;
 
-  Array.from(blobs).forEach(blob => {
-    blob.animate({
-      left: `${clientX}px`,
-      top: `${clientY}px`
-    }, { duration: 3000, fill: "forwards" });
-  });
+  blob1.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
+
+  blob2.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
 }
